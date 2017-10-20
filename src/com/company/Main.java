a677185f5c0dac631c08a2540fe003f1cadab46a
Azzzik <polskoy.st@gmail.com>
2017-10-20 19:20:18 +0300

    First try

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,19 @@
 package com.company;
-
+import java.util.Scanner;
+import java.util.ArrayList;
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+Scanner num = new Scanner(System.in);
+        ArrayList<Integer> arr = new ArrayList<>();
+        while(true) {
+            int a = num.nextInt();
+
+            if(instanceof(a)==Integer){arr.add(a);}else break;
+
+        }
+        System.out.println(arr);
     }
 }
+

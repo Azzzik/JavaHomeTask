a67715b35e2ed9e3944889a0c4584750e475bbd7
Azzzik <polskoy.st@gmail.com>
2017-10-23 13:30:58 +0300

    Adding method complete

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,16 +1,20 @@
 package com.company;
 import java.util.Scanner;
 import java.util.ArrayList;
+import java.lang.String;
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-Scanner num = new Scanner(System.in);
-        ArrayList<Integer> arr = new ArrayList<>();
-        while(true) {
-            int a = num.nextInt();
+Scanner input = new Scanner(System.in);
+int b;
+ArrayList<Integer> arr = new ArrayList<>();
+System.out.println("Input your integers, than press '='");
+        while (true) {
+            String a = input.nextLine();
+            if(a.equals("=")){break;}else {  b = Integer.parseInt(a);}
 
-            if(instanceof(a)==Integer){arr.add(a);}else break;
+            arr.add(b);
 
         }
         System.out.println(arr);

3011d9ecff5f420a0533a513fdad84acae13652c
Azzzik <polskoy.st@gmail.com>
2017-10-25 14:40:07 +0300

    Class Student

---
+++ src/com/company/Student.java
@@ -0,0 +1,10 @@
+package sample;
+
+import java.util.Date;
+
+public class Student {
+    String name;
+    Date birthDate;
+    enum major {MATH, COMPUTER_SCIENECE, LITERATURE, NATURAL_HISTORY};
+    char gender;
+}

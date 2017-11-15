a60ab313588c6a9b7f618fde5e0708de6e96f6fd
Azzzik <polskoy.st@gmail.com>
2017-11-15 19:05:54 +0200

    minorImprovements

--- src/com/company/NameSorter.java
+++ src/com/company/NameSorter.java
@@ -4,14 +4,14 @@ package com.company;
 import java.util.ArrayList;
 
 public class NameSorter {
-    public static void getSorted( ArrayList<Integer> a){
-for (int i =0; i < a.size();i++){
-    int e = a.get(i);
+    public static void getSorted( ArrayList<Integer> arr){
+for (int i =0; i < arr.size();i++){
+    int e = arr.get(i);
     int ins = i;
-    while(ins>0 && a.get(ins-1)>e){
-        a.set(ins, a.get (ins-1) );
+    while(ins>0 && arr.get(ins-1)>e){
+        arr.set(ins, arr.get (ins-1) );
         ins--;
-        a.set(ins, e);
+        arr.set(ins, e);
     }
 }
 
---
+++ src/com/company/NewNameSorter.java
@@ -0,0 +1,11 @@
+package com.company;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+
+public class NewNameSorter {
+    public static void getSorted(ArrayList<Integer> arr, Comparator comp){
+
+
+    }
+}

4ce921981c223d42534456c8ef25eb8eb0eb7119
Azzzik <polskoy.st@gmail.com>
2017-11-16 19:53:42 +0200

    Sorting

--- src/com/company/NameSorter.java
+++ src/com/company/NameSorter.java
@@ -6,12 +6,12 @@ import java.util.ArrayList;
 public class NameSorter {
     public static void getSorted( ArrayList<Integer> arr){
 for (int i =0; i < arr.size();i++){
-    int e = arr.get(i);
+    int elem = arr.get(i);
     int ins = i;
-    while(ins>0 && arr.get(ins-1)>e){
+    while(ins>0 && arr.get(ins-1)>elem){
         arr.set(ins, arr.get (ins-1) );
         ins--;
-        arr.set(ins, e);
+        arr.set(ins, elem);
     }
 }
 
--- src/com/company/NewNameSorter.java
+++ src/com/company/NewNameSorter.java
@@ -4,8 +4,16 @@ import java.util.ArrayList;
 import java.util.Comparator;
 
 public class NewNameSorter {
-    public static void getSorted(ArrayList<Integer> arr, Comparator comp){
+    public static void getSorted(ArrayList<Integer> arr, Comparator comp) {
 
-
+        for (int i = 0; i < arr.size(); i++) {
+            int elem = arr.get(i);
+            int ins = i;
+            while (ins > 0 && comp.compare(ins, ins+1)<0) {
+                arr.set(ins, arr.get(ins - 1));
+                ins--;
+                arr.set(ins, elem);
+            }
+        }
     }
 }

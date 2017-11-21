86201c51a76252c8751fbff961383b9c78924122
Azzzik <polskoy.st@gmail.com>
2017-11-21 20:05:43 +0200

    Enum fix

--- src/com/company/Student.java
+++ src/com/company/Student.java
@@ -5,14 +5,17 @@ import java.util.Date;
 public class Student {
     private String name;
     private String birthDate;
-    private enum major {MATH, COMPUTER_SCIENECE, LITERATURE, NATURAL_HISTORY}
+    private enum Major {MATH, COMPUTER_SCIENECE, LITERATURE, NATURAL_HISTORY}
     private char gender;
+    private Major major;
 
     public String getName(){return name;}
     public String getBirthDate(){return birthDate;}
     public char getGender(){return gender;}
+    public Major getMajor(){return major;}
+
     public void setName(String sname){name=sname;}
     public void setBirthDate(String sbirthdate){birthDate=sbirthdate;}
     public void setGender(char cgender){gender=cgender;}
-    public void setMajor(String emajor){major=emajor;}
+    public void setMajor(Major emajor){major=emajor;}
 }

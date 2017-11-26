a1ae5f0b69e5315e8f304de895e23fad338b93f1
Azzzik <polskoy.st@gmail.com>
2017-11-26 19:32:23 +0200

    Sorting finally implemented

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,5 +1,6 @@
 package com.company;
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.Scanner;
 
 import java.lang.String;
@@ -30,7 +31,53 @@ Student stud0 = new Student();
 stud0.setName("Vasil Vasilenko");
 stud0.setBirthDate("16.07.1991");
 stud0.setGender('M');
-stud0.setMajor(Student.Major.COMPUTER_SCIENECE);
+stud0.setMajor(Student.Major.COMPUTER_SCIENCE);
+
+        Student stud1 = new Student();
+        stud1.setName("Vasylyna Vasiliv");
+        stud1.setBirthDate("13.08.1990");
+        stud1.setGender('F');
+        stud1.setMajor(Student.Major.NATURAL_HISTORY);
+
+        Student stud2 = new Student();
+        stud2.setName("Ivan Ivaniv");
+        stud2.setBirthDate("26.11.1991");
+        stud2.setGender('M');
+        stud2.setMajor(Student.Major.MATH);
+
+        Student stud3 = new Student();
+        stud3.setName("Ivanna Ivanova");
+        stud3.setBirthDate("13.06.1992");
+        stud3.setGender('F');
+        stud3.setMajor(Student.Major.LITERATURE);
+
+        Student stud4 = new Student();
+        stud4.setName("Dmytro Dmytrenko");
+        stud4.setBirthDate("01.01.1991");
+        stud4.setGender('M');
+        stud4.setMajor(Student.Major.COMPUTER_SCIENCE);
+
+        Student stud5 = new Student();
+        stud5.setName("Jaryna Jarova");
+        stud5.setBirthDate("19.12.1990");
+        stud5.setGender('M');
+        stud5.setMajor(Student.Major.MATH);
+
+        ArrayList<Student> list = new ArrayList<>();
+
+        list.add(stud0);
+        list.add(stud1);
+        list.add(stud2);
+        list.add(stud3);
+        list.add(stud4);
+        list.add(stud5);
+
+        System.out.println(list);
+        Comparator comp = new NameComparator() {};
+
+        NameSorter.getSorted(list,comp);
+
+        System.out.println(list);
     }
 }
 
--- src/com/company/NameSorter.java
+++ src/com/company/NameSorter.java
@@ -2,13 +2,17 @@ package com.company;
 
 
 import java.util.ArrayList;
+import java.util.Comparator;
 
 public class NameSorter {
-    public static void getSorted( ArrayList<Integer> arr){
-for (int i =0; i < arr.size();i++){
-    int elem = arr.get(i);
+    public static void getSorted(ArrayList<Student> arr, Comparator comp){
+for (int i =1; i < arr.size();i++){
+
     int ins = i;
-    while(ins>0 && arr.get(ins-1)>elem){
+    Student elem = arr.get(i);
+    Student elem1= arr.get(ins-1);
+
+    while(ins>0 && comp.compare(elem,elem1)>=0 /*arr.get(ins-1)>elem*/){
         arr.set(ins, arr.get (ins-1) );
         ins--;
         arr.set(ins, elem);
